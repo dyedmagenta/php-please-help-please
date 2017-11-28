71ee7c0ac7f3127f47454be3d0c3d0455074c572
Leon Omelan <rozpierog@gmail.com>
2017-11-28 11:05:45 +0100

    working login

--- login.php
+++ login.php
@@ -11,16 +11,18 @@
 				<div class='centered'>
 				<div class='row>
   				<div class='col-lg-6'>
-		        <div class='input-group'>
-							<span class='input-group-addon' id='sizing-addon2' >@</span>
-		          <input type='text' class='form-control' placeholder='Username' aria-describedby='sizing-addon2'>
-							<br>
-							<span class='input-group-addon' id='sizing-addon2'>🔒</span>
-		          <input type='password' class='form-control' placeholder='Username' aria-describedby='sizing-addon2'>
-							<span class='input-group-btn'>
-		        		<button class='btn btn-secondary' type='button'>Go!</button>
-		      		</span>
-		        </div>
+						<form action='api/logIn.php' method='post'>
+			        <div class='input-group'>
+								<span class='input-group-addon' id='sizing-addon2' >@</span>
+			          <input type='text' name='name' class='form-control' placeholder='Username' aria-describedby='sizing-addon2' required>
+								<br>
+								<span class='input-group-addon' id='sizing-addon2'>🔒</span>
+			          <input type='password' name='password' class='form-control' placeholder='Password' aria-describedby='sizing-addon2' required>
+								<span class='input-group-btn'>
+			        		<button class='btn btn-secondary'  type='submit'>Go!</button>
+			      		</span>
+			        </div>
+						</form>
 					</div>
       </div>
 			</div>";
